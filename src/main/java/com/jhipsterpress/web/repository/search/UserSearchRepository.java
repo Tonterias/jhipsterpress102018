ab4c3a6e80f0b5eaf8881220f05cea41d8d8676a
Tonterias <ecorreos@htomail.com>
2018-11-08 12:18:05 +0100

    Initial application generated by JHipster-5.6.1

---
+++ src/main/java/com/jhipsterpress/web/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.jhipsterpress.web.repository.search;
+
+import com.jhipsterpress.web.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
